09f62e00f43a2ba23f79832de1faf4b4bde8a4f5
arif rahman fauzi <arif.rahman1780@gmail.com>
2023-04-09 13:42:42 +0700

    modify layout navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('style-fe/style-fe.css')}}">
-{{--    <title>Fun Bahasa #BahasaAdalahKita</title>--}}
     @yield('title','Fun Bahasa #BahasaAdalahKita')
     <link rel="shortcut icon" href="{{asset('assets/img/funbahasa.png')}}" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
